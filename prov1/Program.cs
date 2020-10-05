b4926b3c405dbef7a500eafb759ade136b449716
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-10-05 08:56:03 +0200

    första committen

---
+++ prov1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace prov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 0;
+            int answerInt = 0;
+            string answerString = "";
+            bool correctAnswer = false;
+            Console.WriteLine("");
+
+
+            while (!correctAnswer)
+            {
+
+                answerString = Console.ReadLine();
+                correctAnswer = int.TryParse(answerString, out answerInt);
+
+                if (!correctAnswer)
+                {
+                    Console.WriteLine("Skriv endast siffror");
+                }
+
+                if (answerInt = 5 || 6 || 8 || 8)
+                {
+                    Console.WriteLine("Near miss!");
+                }
+
+                else if (answerInt != 5)
+                {
+                    Console.WriteLine("Fel! Försök igen.");
+                    correctAnswer = false;
+                }
+                else
+                {
+                    points++;
+                    Console.WriteLine("Hit!.");
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
